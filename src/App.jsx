9b7e577b2a6176d64f2dbe9a355727eea00f1a4a
bruneecoo <bruno12zxz@gmail.com>
2025-05-29 15:34:30 -0300

    primeras coisas

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import UserCard from './components/UserCard'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:3000/peoples')
+    .then((res) => res.json())
+    .then((data) => setUsers(data))
+    .catch((err) => console.error('Erro ao buscar usuario:', err));
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Dashbord de Usuarios</h1>
+      <p>Total de usuarios:</p>
+      <div className='user-container'>
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+        </div>
+        </div>
+);
+}
+export default App;
